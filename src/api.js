85b5a12e218c6f61453bf83927442cc484704a9e
Yeon <dusrksl@gmail.com>
2023-01-29 16:55:58 -0500

    weather-app completed

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "1b1d005f33mshca81e23ffa2d963p1fc292jsn04fb92ca932d",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "e32556872fcd4638e4be173f951ce3c3";
---
+++ src/components/search/search.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                return {
+                    options:
+                        response.data &&
+                        response.data.map((city) => {
+                            return {
+                                value: `${city.latitude} ${city.longitude}`,
+                                label: `${city.name}, ${city.countryCode}`,
+                            };
+                        }),
+                };
+            })
+            .catch((err) => console.error(err));
+    };
+
+    const hanldeOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    };
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debouceTimeout={600}
+            value={search}
+            onChange={hanldeOnChange}
+            loadOptions={loadOptions}
+        />
+    );
+};
+
+export default Search;
